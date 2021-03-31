52db73caa518dc5cdc2401cc2228f8f7b21fede2
Terry Jackson <58715371+jacksoninfosec@users.noreply.github.com>
2021-03-31 01:44:05 -0600

    Update des.py

--- des.py
+++ des.py
@@ -3,4 +3,19 @@
 from des_pbox import *
 from des_sbox import S
 
-print(S(5, 27))
+
+# The Feistel function of DES
+# x is 32 bits
+# key is 48 bits
+# The output is 32 bits
+# Note that 0x3f in binary is six ones: 111111
+def f(x, key):
+	x = pbox(x, E, 32)
+	x ^= key
+	y = 0
+	for i in range(1, 9):
+		z = (x >> 6 * (8 - i)) & 0x3f
+		y <<= 4
+		y ^= S(i, z)
+	return pbox(y, Q, 32)
+
